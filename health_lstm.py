ad904c2c60690a985c9ae1496759cc9bc8ce82a5
gh0stsh0t <rjperias0213@gmail.com>
2018-09-20 10:49:18 +0800

    added manual and auto mode

--- health_lstm.py
+++ health_lstm.py
@@ -2,14 +2,16 @@ import numpy as np
 import pandas as pd
 import sys
 from matplotlib import pyplot as plt
-from sklearn.model_selection import KFold, cross_val_score
+from sklearn.model_selection import KFold, cross_val_score, GridSearchCV, TimeSeriesSplit
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder, StandardScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.svm import SVR
 from keras.models import Sequential
-from keras.layers import Dense, Masking, Dropout, Activation
+from keras.layers import Dense, Dropout, Activation
+from keras.wrappers.scikit_learn import KerasRegressor
 from keras.callbacks import EarlyStopping
 from math import sqrt
+import time
 from datetime import datetime
 from keras import backend as K
 if K.tensorflow_backend._get_available_gpus():
@@ -35,6 +37,29 @@ class Pollution:
         raw_data = pd.read_csv("Data/" + file + ".csv")
         return raw_data
 
+    def model_compile_double(self, optimizer='adam', neurons1=50, neurons2=50, dropout1=0.2, dropout2=0.2):
+        model = Sequential()
+        model.add(CuDNNLSTM(neurons1, input_shape=(1, 16), return_sequences=True))
+        model.add(Dropout(dropout1))
+        model.add(CuDNNLSTM(neurons2))
+        model.add(Dropout(dropout2))
+        model.add(Dense(1))
+        model.add(Activation('linear'))
+        model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['mse'])
+        return model
+
+    def model_compile_triple(self, optimizer='adam', neurons1=50, neurons2=50, neurons3=50, dropout1=0.2, dropout2=0.2):
+        model = Sequential()
+        model.add(CuDNNLSTM(neurons1, input_shape=(1, 16), return_sequences=True))
+        model.add(Dropout(dropout1))
+        model.add(CuDNNLSTM(neurons2, return_sequences=True))
+        model.add(Dropout(dropout2))
+        model.add(CuDNNLSTM(neurons3))
+        model.add(Dense(1))
+        model.add(Activation('linear'))
+        model.compile(loss='mean_squared_error', optimizer=optimizer, metrics=['mse'])
+        return model
+
     def model_fit(self, layers, train_X, train_y, label, test_X=0, epochs=500, optim='rmsprop', batch=10):
         test_X = self.X_test if type(test_X) is int else test_X
         test_y = self.y_test
@@ -59,86 +84,7 @@ class Pollution:
         yhat = svm.predict(X_scaled)
         self.predictor_magtanggol(yhat, "svm")
 
-    def predictor_magtanggol(self, inv_yhat, label):
-        yforms = [self.y,              inv_yhat,
-                  self.y[self.split:], inv_yhat[self.split:],
-                  self.y[:self.split], inv_yhat[:self.split]]
-        self.predictions.append((yforms[1], label))
-        self.loss.append((sqrt(mean_squared_error(yforms[0], yforms[1])), mean_absolute_error(yforms[0], yforms[1]),
-                          sqrt(mean_squared_error(yforms[2], yforms[3])), mean_absolute_error(yforms[2], yforms[3]),
-                          sqrt(mean_squared_error(yforms[4], yforms[5])), mean_absolute_error(yforms[4], yforms[5])))
-
-    def create_loss(self, hist, label):
-        plt.subplot(2, 1, 1)
-        plt.plot(hist.history['loss'], label=label)
-        plt.ylabel('Train Loss')
-        plt.subplot(2, 1, 2)
-        plt.plot(hist.history['val_loss'], label=label)
-        plt.ylabel('Test Loss')
-
-    def main(self, verbosity):
-        self.verbosity = verbosity
-        holder = {}
-        X = pd.DataFrame()
-        for year in range(15, 18):
-            for station in [2, 7, 11, 14]:
-                file = str(station).zfill(2) + '_' + str(year)
-                y = self.parse_all(file)
-                if year in holder:
-                    holder[year] = pd.merge(holder[year], y, how="outer", on="date", sort=False)
-                else:
-                    holder[year] = y
-
-        for key in holder:
-            X = X.append(other=holder[key], ignore_index=True)
-
-        X.to_csv("Data/Features.csv")
-        X.dropna(thresh=6, inplace=True)
-        X.fillna(-1, inplace=True)
-        target_raw = pd.read_csv("Data/MonthlyTarget.csv")
-        weeks = [0 for _ in range(len(target_raw))]
-        for date in X['date'].get_values():
-            which = (int(date[2:-3]) - 1) + (int(date[-2:]) - 15) * 12
-            weeks[which] = weeks[which] + 1
-        print(weeks)
-
-        targets = target_raw['target'].get_values()
-        y = []
-        for ind, people in enumerate(targets):
-            amp = [people // weeks[ind] for _ in range(weeks[ind])]
-            y.extend(amp)
-
-        self.y = pd.Series(y).values.astype('float32').reshape(-1, 1)
-        self.split = (self.y.shape[0] // 3) * 2
-        svm_y = y
-        y = self.scalers[0].fit_transform(self.y)
-        self.y_test = y[self.split:, :]
-        y = y[:self.split, :]
-
-        # X = pd.read_csv("KNN.csv")
-        X = X.drop("date", axis=1)
-        print(X[:5])
-        X = X.values.astype('float32')
-        svm_X=X
-        X = self.scalers[1].fit_transform(X)
-        all_X = X.reshape((X.shape[0], 1, X.shape[1]))
-
-        self.X_test = X[self.split:]
-        self.X_test = self.X_test.reshape((self.X_test.shape[0], 1, self.X_test.shape[1]))
-        X = X[:self.split]
-        X = X.reshape((X.shape[0], 1, X.shape[1]))
-        print("{} {}".format(X.shape, y.shape))
-        startTime = datetime.now()
-
-        # SVM model
-        self.svm_fit(svm_X, svm_y)
-        # Basic feed forward neural network
-        mlp_train_X = self.scalers[1].transform(svm_X[:self.split])
-        mlp_test_X = self.scalers[1].transform(svm_X[self.split:])
-        self.model_fit([Dense(12, input_dim=svm_X.shape[1], activation='relu'),
-                        Dense(8, activation='relu'),
-                        Dense(1)],
-                        train_X=mlp_train_X, train_y=y, test_X=mlp_test_X, optim='adam',epochs=500, batch=2, label="FFDNN")
+    def manual(self, X, y):
         # define and fit model 0
         self.model_fit([CuDNNLSTM(50, input_shape=(X.shape[1], X.shape[2])),
                         Dense(1)],
@@ -268,20 +214,144 @@ class Pollution:
                         Dense(1),
                         Activation('linear')],
                        train_X=X, train_y=y, batch=4, label="sixteenth")
+        # define and fit model BEST
+        self.model_fit([CuDNNLSTM(250, input_shape=(X.shape[1], X.shape[2]), return_sequences=True),
+                        Dropout(0.2),
+                        CuDNNLSTM(200, return_sequences=True),
+                        Dropout(0.2),
+                        CuDNNLSTM(300),
+                        Dense(1),
+                        Activation('linear')],
+                       train_X=X, train_y=y, batch=5, label="best3")
+        # define and fit model BEST
+        self.model_fit([CuDNNLSTM(250, input_shape=(X.shape[1], X.shape[2]), return_sequences=True),
+                        Dropout(0.2),
+                        CuDNNLSTM(200),
+                        Dropout(0.2),
+                        Dense(1),
+                        Activation('linear')],
+                       train_X=X, train_y=y, batch=20, label="best2")
 
-        for ind, x in enumerate(self.times):
-            print("Time for {}: {}".format(ind, x))
-        print("\nTraining time of All Models {}\n".format(datetime.now() - startTime))
+    def predictor_magtanggol(self, inv_yhat, label):
+        yforms = [self.y,              inv_yhat,
+                  self.y[self.split:], inv_yhat[self.split:],
+                  self.y[:self.split], inv_yhat[:self.split]]
+        self.predictions.append((yforms[1], label))
+        self.loss.append((sqrt(mean_squared_error(yforms[0], yforms[1])), mean_absolute_error(yforms[0], yforms[1]),
+                          sqrt(mean_squared_error(yforms[2], yforms[3])), mean_absolute_error(yforms[2], yforms[3]),
+                          sqrt(mean_squared_error(yforms[4], yforms[5])), mean_absolute_error(yforms[4], yforms[5])))
 
-        plt.legend()
-        plt.show()
-        plt.gcf().clear()
+    def create_loss(self, hist, label):
+        plt.subplot(2, 1, 1)
+        plt.plot(hist.history['loss'], label=label)
+        plt.ylabel('Train Loss')
+        plt.subplot(2, 1, 2)
+        plt.plot(hist.history['val_loss'], label=label)
+        plt.ylabel('Test Loss')
 
-        split = (self.split / self.y.shape[0] ) * 100
-        print("Train-Test split: {:.2f} {:.2f}".format(split, 100-split))
-        for index, error in enumerate(self.loss):
-            print('Model #{}\nTotal RMSE: {:.2f}\nTotal MAE: {:.2f}\nTrain RMSE: {:.2f}\nTrain  MAE: {:.2f}\nTest RMSE: {:.2f}\nTest  MAE: {:.2f}\n'.format(index-1, error[0], error[1],error[2], error[3], error[4], error[5]))
+    def main(self, verbosity, auto):
+        self.verbosity = verbosity
+        holder = {}
+        X = pd.DataFrame()
+        for year in range(15, 18):
+            for station in [2, 7, 11, 14]:
+                file = str(station).zfill(2) + '_' + str(year)
+                y = self.parse_all(file)
+                if year in holder:
+                    holder[year] = pd.merge(holder[year], y, how="outer", on="date", sort=False)
+                else:
+                    holder[year] = y
 
+        for key in holder:
+            X = X.append(other=holder[key], ignore_index=True)
+
+        X.to_csv("Data/Features.csv")
+        X.dropna(thresh=6, inplace=True)
+        X.fillna(-1, inplace=True)
+        target_raw = pd.read_csv("Data/MonthlyTarget.csv")
+        weeks = [0 for _ in range(len(target_raw))]
+        for date in X['date'].get_values():
+            which = (int(date[2:-3]) - 1) + (int(date[-2:]) - 15) * 12
+            weeks[which] = weeks[which] + 1
+        print(weeks)
+
+        targets = target_raw['target'].get_values()
+        y = []
+        for ind, people in enumerate(targets):
+            amp = [people // weeks[ind] for _ in range(weeks[ind])]
+            y.extend(amp)
+
+        self.y = pd.Series(y).values.astype('float32').reshape(-1, 1)
+        self.split = (self.y.shape[0] // 3) * 2
+        svm_y = y
+        y = self.scalers[0].fit_transform(self.y)
+        self.y_test = y[self.split:, :]
+        y = y[:self.split, :]
+
+        # X = pd.read_csv("KNN.csv")
+        X = X.drop("date", axis=1)
+        print(X[:5])
+        X = X.values.astype('float32')
+        svm_X=X
+        X = self.scalers[1].fit_transform(X)
+        all_X = X.reshape((X.shape[0], 1, X.shape[1]))
+
+        self.X_test = X[self.split:]
+        self.X_test = self.X_test.reshape((self.X_test.shape[0], 1, self.X_test.shape[1]))
+        X = X[:self.split]
+        X = X.reshape((X.shape[0], 1, X.shape[1]))
+        print("{} {}".format(X.shape, y.shape))
+        startTime = datetime.now()
+
+        # SVM model
+        self.svm_fit(svm_X, svm_y)
+        # Basic feed forward neural network
+        mlp_train_X = self.scalers[1].transform(svm_X[:self.split])
+        mlp_test_X = self.scalers[1].transform(svm_X[self.split:])
+        self.model_fit([Dense(12, input_dim=svm_X.shape[1], activation='relu'),
+                        Dense(8, activation='relu'),
+                        Dense(1)],
+                        train_X=mlp_train_X, train_y=y, test_X=mlp_test_X, optim='adam', epochs=500, batch=2, label="FFDNN")
+        if auto == 0:
+            self.manual(X, y)
+            for ind, x in enumerate(self.times):
+                print("Time for {}: {}".format(ind, x))
+            print("\nTraining time of All Models {}\n".format(datetime.now() - startTime))
+
+            plt.legend()
+            plt.show()
+            plt.gcf().clear()
+
+            split = (self.split / self.y.shape[0]) * 100
+            print("Train-Test split: {:.2f} {:.2f}".format(split, 100 - split))
+            for index, error in enumerate(self.loss):
+                print(
+                    'Model #{}\nTotal RMSE: {:.2f}\nTotal MAE: {:.2f}\nTrain RMSE: {:.2f}\nTrain  MAE: {:.2f}\nTest RMSE: {:.2f}\nTest  MAE: {:.2f}\n'.format(
+                        index - 1, error[0], error[1], error[2], error[3], error[4], error[5]))
+        else:
+            print("Starting GridSearch in 5")
+            time.sleep(5)
+            param_grid = {'neurons1': list(range(100, 325, 75)),
+                          'neurons2': list(range(200, 500, 100)),
+                          # 'neurons3': list(range(200, 500, 150)),
+                          'dropout1': [0.2],
+                          'dropout2': [0.2],
+                          'optimizer': ['adam', 'rmsprop'],
+                          'batch_size': [5, 20, 50]}
+            # model = KerasRegressor(build_fn=self.model_compile_triple, epochs=500, batch_size=10, verbose=verbosity)
+            model = KerasRegressor(build_fn=self.model_compile_double, epochs=500, batch_size=10, verbose=verbosity)
+            my_cv = TimeSeriesSplit(n_splits=2).split(X)
+            grid = GridSearchCV(estimator=model, param_grid=param_grid, cv=my_cv)
+            grid.fit(X, y)
+            print("\nTraining time of All Models {}\n".format(datetime.now() - startTime))
+            print("Best: {:.2f} using {}".format(grid.best_score_, grid.best_params_))
+            yhat = grid.best_estimator_.predict(all_X)
+            yhat = pd.Series(yhat).values.astype('float32').reshape(-1, 1)
+            inv_yhat = self.scalers[0].inverse_transform(yhat)
+            self.predictor_magtanggol(inv_yhat, "Best")
+            plt.legend()
+            plt.show()
+            plt.gcf().clear()
         plt.plot(self.y, label="real")
         for vals in self.predictions:
             plt.plot(vals[0], label=vals[1])
@@ -289,9 +359,15 @@ class Pollution:
         plt.show()
         plt.gcf().clear()
 
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         x = int(sys.argv[1])
+        if len(sys.argv) > 2:
+            y = int(sys.argv[2])
+        else:
+            y = 0
     else:
         x = 0
-    Pollution().main(x)
+        y = 0
+    Pollution().main(x, y)
