6585f89ddd2d331de83bfb920526c38003d209d8
gh0stsh0t <rjperias0213@gmail.com>
2019-04-10 10:44:21 +0800

    Minor change

--- src/webapp.py
+++ src/webapp.py
@@ -92,6 +92,5 @@ if __name__ == "__main__":
     print(("* Loading Keras model and Flask starting server..."
            "please wait until server has fully started"))
     load_models()
-    port = 5000
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
